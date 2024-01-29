0876b837bb1c875a587aa68b62960da26fa0e594
ChinmayJoshi98 <joshichinmay94@gmail.com>
2024-01-29 14:30:18 -0600

    Home and Navbar components updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Games.DJCJ</h1>
+            <div className="title">
+                <h1>Games.DJCJ</h1>
+            </div>
             <div className="links">
-                <Link to='/'>Home</Link>
-                <Link to='/addUser'>Add a User</Link>
+                <Link to='/' className='linkSec'>Home</Link>
+                <Link to='/addUser' className='linkSec'>Add a User</Link>
             </div>
         </nav>
     );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,21 @@
 const Home = () => {
     return ( 
-        <h1>List of all Users</h1>
+        <div className="content">
+            <h1>List of all Users</h1>
+            <div className="temp">
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore dignissimos voluptates dolorem dolores hic repellendus debitis ratione quas quis qui iusto natus, totam recusandae a. Consequuntur corrupti atque expedita!
+            Culpa vel totam voluptas, sunt deleniti recusandae distinctio ipsum quae deserunt ipsa doloremque vero tempora facilis. Consequuntur laudantium sed nam optio placeat minima molestiae facilis laborum alias, error ullam sit.
+            Fuga non, doloremque iusto nobis delectus ex numquam a tenetur amet minima sint explicabo ullam, quaerat quidem, cupiditate quia quas asperiores doloribus quod facilis incidunt quam dicta voluptates. Voluptatibus, ipsam?
+            Magnam quos vitae maiores quam natus ab nulla sit cumque, corrupti, minima sint accusantium velit voluptas? Vel officia accusantium quaerat reprehenderit itaque praesentium quod aliquid, eaque unde repudiandae quae repellat.
+            Aliquam vitae provident corporis, recusandae deleniti illum error. Suscipit, pariatur beatae! Ut, sequi officia pariatur nulla numquam dolores, a, voluptatibus amet officiis quis eum nisi ullam iste doloremque blanditiis alias.
+            Nihil optio cum possimus hic magni voluptatum laudantium esse eveniet temporibus, illo magnam asperiores, mollitia reiciendis cumque porro voluptas nulla fuga doloremque vitae aperiam adipisci! Voluptatem placeat voluptates nisi ipsam.
+            Omnis, quos officia cum doloribus unde culpa ad quaerat suscipit optio dolores pariatur est labore laboriosam debitis deleniti eos molestias dolor quisquam earum et blanditiis accusamus facere, incidunt inventore? Aliquam?
+            Saepe quas odio nulla amet quibusdam eaque eos fugit sint atque officia tempore laudantium, libero ab nihil odit aliquam animi quasi neque nisi quidem similique. Cumque incidunt repellat ipsum exercitationem.
+            Neque repellendus porro labore cupiditate necessitatibus consequuntur ea aliquam fuga, illo perspiciatis veritatis. Dolorem atque et animi cum provident! Labore voluptatibus ut aliquam voluptate eum iste esse velit adipisci amet.
+            Inventore quos aperiam debitis quidem voluptate porro dicta placeat saepe obcaecati. Atque magnam totam quae necessitatibus voluptas? Eaque et facilis doloremque, tenetur laudantium placeat, facere enim maxime reiciendis odit quisquam!</h2>
+            </div>
+        </div>
+        
      );
 }
  
